a4630499e465f13fdb538ad91a3413d4b74fa4a1
Lubo Sladok <lubomir.sladok@bsp.sk>
2020-02-20 14:49:55 +0100

    initial setup

---
+++ app/src/main/java/online/fixu/bsp/alf/notif/MainActivity.java
@@ -0,0 +1,244 @@
+package online.fixu.bsp.alf.notif;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+
+    public static final int NOTIFICATION_ID = 888;
+
+    // RelativeLayout required for SnackBars to alert users when Notifications are disabled for app.
+//    private RelativeLayout mMainRelativeLayout;
+
+    private NotificationManagerCompat mNotificationManagerCompat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        mMainRelativeLayout = (RelativeLayout) findViewById(R.id.mainRelativeLayout);
+        mNotificationManagerCompat = NotificationManagerCompat.from(getApplicationContext());
+
+
+    }
+
+    public void onClick(View view) {
+
+        Log.d(TAG, "onClick()");
+
+//        boolean areNotificationsEnabled = mNotificationManagerCompat.areNotificationsEnabled();
+
+//        if (!areNotificationsEnabled) {
+//            // Because the user took an action to create a notification, we create a prompt to let
+//            // the user re-enable notifications for this application again.
+//            Snackbar snackbar = Snackbar
+//                    .make(
+//                            mMainRelativeLayout,
+//                            "You need to enable notifications for this app",
+//                            Snackbar.LENGTH_LONG)
+//                    .setAction("ENABLE", new View.OnClickListener() {
+//                        @Override
+//                        public void onClick(View view) {
+//                            // Links to this app's notification settings
+//                            openNotificationSettingsForApp();
+//                        }
+//                    });
+//            snackbar.show();
+//            return;
+//        }
+        generateBigTextStyleNotification();
+    }
+
+    /*
+     * Generates a BIG_TEXT_STYLE Notification that supports both phone/tablet and wear. For devices
+     * on API level 16 (4.1.x - Jelly Bean) and after, displays BIG_TEXT_STYLE. Otherwise, displays
+     * a basic notification.
+     */
+    private void generateBigTextStyleNotification() {
+
+        Log.d(TAG, "generateBigTextStyleNotification()");
+
+        // Main steps for building a BIG_TEXT_STYLE notification:
+        //      0. Get your data
+        //      1. Create/Retrieve Notification Channel for O and beyond devices (26+)
+        //      2. Build the BIG_TEXT_STYLE
+        //      3. Set up main Intent for notification
+        //      4. Create additional Actions for the Notification
+        //      5. Build and issue the notification
+
+        // 0. Get your data (everything unique per Notification).
+        MockDatabase.BigTextStyleReminderAppData bigTextStyleReminderAppData =
+                MockDatabase.getBigTextStyleData();
+
+        // 1. Create/Retrieve Notification Channel for O and beyond devices (26+).
+        String notificationChannelId =
+                NotificationUtil.createNotificationChannel(this, bigTextStyleReminderAppData);
+
+
+        // 2. Build the BIG_TEXT_STYLE.
+        NotificationCompat.BigTextStyle bigTextStyle = new NotificationCompat.BigTextStyle()
+                // Overrides ContentText in the big form of the template.
+                .bigText(bigTextStyleReminderAppData.getBigText())
+                // Overrides ContentTitle in the big form of the template.
+                .setBigContentTitle(bigTextStyleReminderAppData.getBigContentTitle())
+                // Summary line after the detail section in the big form of the template.
+                // Note: To improve readability, don't overload the user with info. If Summary Text
+                // doesn't add critical information, you should skip it.
+                .setSummaryText(bigTextStyleReminderAppData.getSummaryText());
+
+
+        // 3. Set up main Intent for notification.
+        Intent notifyIntent = new Intent(this, BigTextMainActivity.class);
+
+        // When creating your Intent, you need to take into account the back state, i.e., what
+        // happens after your Activity launches and the user presses the back button.
+
+        // There are two options:
+        //      1. Regular activity - You're starting an Activity that's part of the application's
+        //      normal workflow.
+
+        //      2. Special activity - The user only sees this Activity if it's started from a
+        //      notification. In a sense, the Activity extends the notification by providing
+        //      information that would be hard to display in the notification itself.
+
+        // For the BIG_TEXT_STYLE notification, we will consider the activity launched by the main
+        // Intent as a special activity, so we will follow option 2.
+
+        // For an example of option 1, check either the MESSAGING_STYLE or BIG_PICTURE_STYLE
+        // examples.
+
+        // For more information, check out our dev article:
+        // https://developer.android.com/training/notify-user/navigation.html
+
+        // Sets the Activity to start in a new, empty task
+        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        PendingIntent notifyPendingIntent =
+                PendingIntent.getActivity(
+                        this,
+                        0,
+                        notifyIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+
+
+        // 4. Create additional Actions (Intents) for the Notification.
+
+        // In our case, we create two additional actions: a Snooze action and a Dismiss action.
+        // Snooze Action.
+        Intent snoozeIntent = new Intent(this, BigTextIntentService.class);
+        snoozeIntent.setAction(BigTextIntentService.ACTION_SNOOZE);
+
+        PendingIntent snoozePendingIntent = PendingIntent.getService(this, 0, snoozeIntent, 0);
+        NotificationCompat.Action snoozeAction =
+                new NotificationCompat.Action.Builder(
+                        R.drawable.ic_alarm_white_48dp,
+                        "Snooze",
+                        snoozePendingIntent)
+                        .build();
+
+
+        // Dismiss Action.
+        Intent dismissIntent = new Intent(this, BigTextIntentService.class);
+        dismissIntent.setAction(BigTextIntentService.ACTION_DISMISS);
+
+        PendingIntent dismissPendingIntent = PendingIntent.getService(this, 0, dismissIntent, 0);
+        NotificationCompat.Action dismissAction =
+                new NotificationCompat.Action.Builder(
+                        R.drawable.ic_cancel_white_48dp,
+                        "Dismiss",
+                        dismissPendingIntent)
+                        .build();
+
+
+        // 5. Build and issue the notification.
+
+        // Because we want this to be a new notification (not updating a previous notification), we
+        // create a new Builder. Later, we use the same global builder to get back the notification
+        // we built here for the snooze action, that is, canceling the notification and relaunching
+        // it several seconds later.
+
+        // Notification Channel Id is ignored for Android pre O (26).
+        NotificationCompat.Builder notificationCompatBuilder =
+                new NotificationCompat.Builder(
+                        getApplicationContext(), notificationChannelId);
+
+        GlobalNotificationBuilder.setNotificationCompatBuilderInstance(notificationCompatBuilder);
+
+        Notification notification = notificationCompatBuilder
+                // BIG_TEXT_STYLE sets title and content for API 16 (4.1 and after).
+                .setStyle(bigTextStyle)
+                // Title for API <16 (4.0 and below) devices.
+                .setContentTitle(bigTextStyleReminderAppData.getContentTitle())
+                // Content for API <24 (7.0 and below) devices.
+                .setContentText(bigTextStyleReminderAppData.getContentText())
+                .setSmallIcon(R.drawable.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(
+                        getResources(),
+                        R.drawable.ic_alarm_white_48dp))
+                .setContentIntent(notifyPendingIntent)
+                .setDefaults(NotificationCompat.DEFAULT_ALL)
+                // Set primary color (important for Wear 2.0 Notifications).
+                .setColor(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary))
+
+                // SIDE NOTE: Auto-bundling is enabled for 4 or more notifications on API 24+ (N+)
+                // devices and all Wear devices. If you have more than one notification and
+                // you prefer a different summary notification, set a group key and create a
+                // summary notification via
+                // .setGroupSummary(true)
+                // .setGroup(GROUP_KEY_YOUR_NAME_HERE)
+
+                .setCategory(Notification.CATEGORY_REMINDER)
+
+                // Sets priority for 25 and below. For 26 and above, 'priority' is deprecated for
+                // 'importance' which is set in the NotificationChannel. The integers representing
+                // 'priority' are different from 'importance', so make sure you don't mix them.
+                .setPriority(bigTextStyleReminderAppData.getPriority())
+
+                // Sets lock-screen visibility for 25 and below. For 26 and above, lock screen
+                // visibility is set in the NotificationChannel.
+                .setVisibility(bigTextStyleReminderAppData.getChannelLockscreenVisibility())
+
+                // Adds additional actions specified above.
+                .addAction(snoozeAction)
+                .addAction(dismissAction)
+
+                .build();
+
+        mNotificationManagerCompat.notify(NOTIFICATION_ID, notification);
+    }
+
+    /**
+     * Helper method for the SnackBar action, i.e., if the user has this application's notifications
+     * disabled, this opens up the dialog to turn them back on after the user requests a
+     * Notification launch.
+     *
+     * IMPORTANT NOTE: You should not do this action unless the user takes an action to see your
+     * Notifications like this sample demonstrates. Spamming users to re-enable your notifications
+     * is a bad idea.
+     */
+    private void openNotificationSettingsForApp() {
+        // Links to this app's notification settings.
+        Intent intent = new Intent();
+        intent.setAction("android.settings.APP_NOTIFICATION_SETTINGS");
+        intent.putExtra("app_package", getPackageName());
+        intent.putExtra("app_uid", getApplicationInfo().uid);
+        startActivity(intent);
+    }
+}
